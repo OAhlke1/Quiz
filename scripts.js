1b465319561ca40890257e76410a8cf735f6209d
OAhlke1 <oahlke1@gmail.com>
2024-05-12 12:47:09 +0200

    first-commit

---
+++ scripts.js
@@ -0,0 +1,349 @@
+let cardContents = [
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+    {
+        topImg: './images/mathe.jpg',
+        question: `Was erhält man, wenn man
+                  <br>1/1 + 1/2 + 1/3 + ...
+                  <br>bis zur Unendlichkeit addiert?`,
+        answers: {
+                    wrongAnswers: ['100π', '-1/12', 'man weiß es bis heute nicht'],
+                    correctAnswer: 'Unendlich'
+                },
+        infoText: `Tatsächlich ist es unendlich!
+                  <br><a href="https://de.wikibooks.org/wiki/Mathe_f%C3%BCr_Nicht-Freaks:_Harmonische_Reihe" target="_blank">Hier geht es zum Beweis</a>`
+    },
+];
+let i = 0;
+let randomNumber = 0;
+let correctlyAnswered = 0;
+let widthPerceBar = 0;
+let questionsAmount = cardContents.length;
+let cardContentTemplate = /* HTML */ `
+    <div class="imageTop disFlexColumnCenter">
+        <img src="${cardContents[i].topImg}" class="card-img-top" alt="Mathe-Bild">
+    </div>
+    <div class="card-body disFlexColumnCenter">
+    <h5 class="card-title">
+        ${cardContents[i].question}
+    </h5>
+    <div class="answerBlock disFlexColumnCenter">
+        <a href="#" class="btn btn-primary"></a>
+        <a href="#" class="btn btn-primary"></a>
+        <a href="#" class="btn btn-primary"></a>
+        <a href="#" class="btn btn-primary"></a>
+    </div>
+        <div class="extraInfo displayNone disFlexColumnCenter">
+            <p>
+                ${cardContents[i].infoText}
+            </p>
+        </div>
+    </div>
+`;
+let clickAnsw = function clickAnswer(event) {
+    event.preventDefault();
+    if(document.querySelector('.card').classList.contains('answered')) {
+        return;
+    }else {
+        document.querySelector('.card').classList.add('answered');
+        if(event.target.classList.contains('correct')) {
+            event.target.style.backgroundColor = 'green'
+            event.target.style.color = 'white';
+            correctlyAnswered++;
+        }else {
+            event.target.style.backgroundColor = 'red';
+            event.target.style.color = 'white';
+            document.querySelector('.answerBlock a.correct').style.backgroundColor = 'green';
+            document.querySelector('.answerBlock a.correct').style.color = "white";
+            document.querySelector('.extraInfo').classList.remove('displayNone');
+        }
+    }
+    perceBarLength();
+}
+
+function renderCard() {
+    document.querySelector('.nextQuestion').classList.add('displayNone');
+    document.querySelector('.card').innerHTML = '';
+    document.querySelector('.card').innerHTML = cardContentTemplate;
+    document.querySelector('.card').classList.remove('answered');
+    setCorrectWrong();
+    addAnswerClick();
+}
+
+function setCorrectWrong() {
+    let answers = document.querySelectorAll('.answerBlock a');
+    for(let k=0; k<4; k++) {
+        answers[k].classList.add('wrong');
+        answers[k].addEventListener('click', clickAnsw);
+    }
+    randomNumber = parseInt(Math.floor(Math.random()*4));
+    answers[randomNumber].classList.remove('wrong');
+    answers[randomNumber].classList.add('correct');
+    setAnswers();
+}
+
+function setAnswers() {
+    let wrongAnswers = document.querySelectorAll('.answerBlock a.wrong');
+    for(let j=0; j<cardContents[i].answers.wrongAnswers.length; j++) {
+        wrongAnswers[j].innerHTML = cardContents[i].answers.wrongAnswers[j];
+    }
+    document.querySelector('.answerBlock a.correct').innerHTML = cardContents[i].answers.correctAnswer;
+}
+
+function perceBarLength(perce = 0) {
+    perce++;
+    if(perce < 101) {
+        if(perce === 1) {
+            document.querySelector('.perceBar .inner').querySelector('p').innerHTML = (i+1)+"/"+questionsAmount;
+        }
+        widthPerceBar += (perce/questionsAmount/50);
+        document.querySelector('.perceBar .inner').style.width = `${widthPerceBar}%`;
+        setTimeout(()=>{perceBarLength(perce)}, 1);
+    }else {
+        document.querySelector('.nextQuestion').classList.remove('displayNone');
+        return;
+    }
+}
+
+function nextCard() {
+    i++;
+    if(i === questionsAmount) {
+        document.querySelector('.card').classList.add("displayNone");
+        document.querySelector('.resultsScreen').remove("displayNone");
+    }else {
+        renderCard();
+    }
+}
+
+function addAnswerClick() {
+    console.log('Hi');
+    for(let l=0; l<4; l++) {
+        document.querySelectorAll('.answerBlock a')[l].addEventListener('click', clickAnsw);
+    }
+}
